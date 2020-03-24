ec24a6c94ec7b74294c339b027f79784e5725736
lennartdeknikker <me@lenn4rt.com>
2020-03-24 19:01:33 +0100

    add css minification

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,8 @@
 // build script inspired by https://www.webdevdrops.com/en/build-static-site-generator-nodejs-8969ebe34b22/
-const fse = require('fs-extra');
+const fs = require('fs-extra');
 const path = require('path');
+const minifyStream = require('minify-stream');
+const minifyCssStream = require('minify-css-stream');
 const { promisify } = require('util');
 const ejsRenderFile = promisify(require('ejs').renderFile);
 const globP = promisify(require('glob'));
@@ -9,9 +11,29 @@ const createDataObject = require('../site.config');
 const srcPath = './src';
 const distPath = './public';
 
-fse.emptyDirSync(distPath);
+fs.emptyDirSync(distPath);
+fs.copy(`${srcPath}/assets`, distPath);
 
-fse.copy(`${srcPath}/assets`, distPath);
+function minifyScripts() {
+  fs.mkdir(`${distPath}/scripts`);
+  const readStream = fs.createReadStream(`${srcPath}/assets/scripts/countdown.js`);
+  const WriteStream = fs.createWriteStream(`${distPath}/scripts/countdown.min.js`);
+
+  readStream.pipe(minifyStream())
+    .pipe(WriteStream);
+}
+
+function minifyCss() {
+  fs.mkdir(`${distPath}/stylesheets`);
+  const readStream = fs.createReadStream(`${srcPath}/assets/stylesheets/main.css`);
+  const WriteStream = fs.createWriteStream(`${distPath}/stylesheets/main.min.css`);
+
+  readStream.pipe(minifyCssStream())
+    .pipe(WriteStream);
+}
+
+minifyScripts();
+minifyCss();
 
 async function generatePages() {
   globP('**/*.ejs', { cwd: `${srcPath}/pages` })
@@ -21,11 +43,11 @@ async function generatePages() {
         const fileData = path.parse(file);
         const destPath = path.join(distPath, fileData.dir);
 
-        fse.mkdirs(destPath)
+        fs.mkdirs(destPath)
           .then(() => ejsRenderFile(`${srcPath}/pages/${file}`, { ...config }))
           .then((pageContents) => ejsRenderFile(`${srcPath}/layouts/index.ejs`, { ...config, body: pageContents }))
           .then((layoutContent) => {
-            fse.writeFile(`${destPath}/${fileData.name}.html`, layoutContent);
+            fs.writeFile(`${destPath}/${fileData.name}.html`, layoutContent);
           })
           .catch((err) => { console.error(err); });
       });
